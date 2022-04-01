a830ba9c4b95dc9588cd6c10fd2e6215a5f54320
Daniel Merkl <daniel.merkl.dm@gmail.com>
2022-04-01 15:35:23 +0200

    implement custom hook to fetch events

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,25 @@
+import { useEffect } from 'react';
+
+import { useEvents } from './hooks/useEvents';
+import { api } from './utils/api';
+import { Event } from './types/interface/event';
+import { EventsTable } from './components/EventsTable';
+
 export function App() {
-  return <h1>cleanup</h1>;
+  const { data: events } = useEvents();
+
+  useEffect(() => {
+    const event: Omit<Event, 'id'> = {
+      title: 'Banane',
+      city: 'Paris',
+      date: new Date(),
+    };
+    api.post('/events', event);
+  }, []);
+
+  return (
+    <main>
+      <EventsTable events={events ?? []} />
+    </main>
+  );
 }
---
+++ src/hooks/useEvents.ts
@@ -0,0 +1,19 @@
+import { useQuery } from 'react-query';
+import { AxiosError, AxiosResponse } from 'axios';
+
+import { api } from '../utils/api';
+import { Event } from '../types/interface/event';
+import { EventDto } from '../types/interface/eventDto';
+
+export const useEvents = () => {
+  return useQuery<Event[], AxiosError>('/events', () => {
+    return api
+      .get('/events')
+      .then((res: AxiosResponse<EventDto[]>) =>
+        res.data.map((eventDto) => ({
+          ...eventDto,
+          date: new Date(eventDto.date),
+        })),
+      );
+  });
+};
