ca70bc6af0efa0ce7623b2f4d4f936f805c819c2
Daniel Merkl <daniel.merkl.dm@gmail.com>
2022-04-01 17:34:29 +0200

    inline interface

--- src/hooks/useEvents.ts
+++ src/hooks/useEvents.ts
@@ -3,7 +3,13 @@ import { AxiosError, AxiosResponse } from 'axios';
 
 import { api } from '../utils/api';
 import { Event } from '../types/interface/event';
-import { EventDto } from '../types/interface/eventDto';
+
+interface EventDto {
+  id: string;
+  title: string;
+  date: string;
+  city: string;
+}
 
 export const useEvents = () => {
   return useQuery<Event[], AxiosError>('events', () => {
--- src/types/interface/eventDto.ts
+++
@@ -1,6 +0,0 @@
-export interface EventDto {
-  id: string;
-  title: string;
-  date: string;
-  city: string;
-}
