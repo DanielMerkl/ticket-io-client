fd9dbbce03c5ba0c2c957794caf40a3cf5e0d6f2
Daniel Merkl <daniel.merkl.dm@gmail.com>
2022-04-01 17:17:54 +0200

    fix typo

--- src/hooks/useEvents.ts
+++ src/hooks/useEvents.ts
@@ -6,14 +6,12 @@ import { Event } from '../types/interface/event';
 import { EventDto } from '../types/interface/eventDto';
 
 export const useEvents = () => {
-  return useQuery<Event[], AxiosError>('/events', () => {
-    return api
-      .get('/events')
-      .then((res: AxiosResponse<EventDto[]>) =>
-        res.data.map((eventDto) => ({
-          ...eventDto,
-          date: new Date(eventDto.date),
-        })),
-      );
+  return useQuery<Event[], AxiosError>('events', () => {
+    return api.get('/events').then((res: AxiosResponse<EventDto[]>) =>
+      res.data.map((eventDto) => ({
+        ...eventDto,
+        date: new Date(eventDto.date),
+      })),
+    );
   });
 };
